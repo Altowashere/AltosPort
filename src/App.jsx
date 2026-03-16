c9679881517b10d94afce94501b40f3395aecb44
Altowashere <your.email@example.com>
2026-03-16 16:38:46 -0500

    Deleted some stuff and added a feature so reloading brings the user to the top.

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,16 @@ import Navbar from "./components/layout/Navbar";
 import Hero from "./components/sections/hero";
 import About from "./components/sections/about";
 import WipBanner from "./components/ui/WipBanner";
+import { useEffect } from "react";
+
 
 
 const App = () => {
+    useEffect(() => {
+      window.history.scrollRestoration = "manual";
+      window.scrollTo(0, 0);
+    }, []);
+
   return (
     <div className="min-h-screen bg-black pb-[180vh]">
       <WipBanner /> 
