c235fb689889a91ea3a5300761aad3129555d67d
Altowashere <your.email@example.com>
2026-03-16 10:59:13 -0500

    set up the wip progress banner, might rewrite it in the near future

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import Navbar from "./components/layout/Navbar";
 import Hero from "./components/sections/hero";
 import About from "./components/sections/about";
+import WipBanner from "./components/ui/WipBanner";
+
 
 const App = () => {
   return (
     <div className="min-h-screen bg-black pb-[180vh]">
+      <WipBanner />
       <Navbar />
       <main>
         <Hero />
